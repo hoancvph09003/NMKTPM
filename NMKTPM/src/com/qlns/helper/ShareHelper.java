c3e8e719b2e107f590d6e2a9d08036e6b360e6f0
hoancvph09003 <51955721+hoancvph09003@users.noreply.github.com>
2020-02-23 18:02:27 +0700

    taoproject
    taoproject

---
+++ NMKTPM/src/com/qlns/helper/ShareHelper.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.qlns.helper;
+
+import java.awt.Image;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author Hoanf
+ */
+public class ShareHelper {
+
+    public static final Image APP_ICON;
+
+    static {
+        String file = "/com/polypro/icon/fpt.png";
+        APP_ICON = new ImageIcon(ShareHelper.class.getResource(file)).getImage();
+    }
+
+    /*
+     * Sao chép file logo chuyên đề vào thư mục logo
+     *
+     * @param file là đối tượng file ảnh
+     * @return chép được hay không
+     */
+    public static boolean saveLogo(File file) {
+        File dir = new File("logos");
+        // Tạo thư mục nếu chưa tồn tại
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        File newFile = new File(dir, file.getName());
+        try {
+            // Copy vào thư mục logos (đè nếu đã tồn tại)
+            Path source = Paths.get(file.getAbsolutePath());
+            Path destination = Paths.get(newFile.getAbsolutePath());
+            Files.copy(source, destination, StandardCopyOption.REPLACE_EXISTING);
+            return true;
+        } catch (Exception ex) {
+            return false;
+        }
+    }
+
+    /*
+     * Đọc hình ảnh logo chuyên đề
+     *
+     * @param fileName là tên file logo
+     * @return ảnh đọc được
+     */
+    public static ImageIcon readLogo(String fileName) {
+        File path = new File("logos", fileName);
+        return new ImageIcon(path.getAbsolutePath());
+    }
+
+}
